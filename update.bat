8d88ad0a17b4d4606a7d908a2225f52af92ff43d
SaiSaaketh <75871456+SaiSaaketh@users.noreply.github.com>
2021-04-19 10:31:30 +0530

    Initial commit

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\JAGADEESWARARAO\OneDrive\Documents\GitHub\hoverAssist
+cd C:\Users\JAGADEESWARARAO\HoverAssist
 git add --all
 git commit -m "Initial commit"
 git push -u origin main
