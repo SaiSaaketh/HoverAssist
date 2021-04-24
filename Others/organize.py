a7c40b16c1f7ae179f0038f471baec7a74b72584
SaiSaaketh <75871456+SaiSaaketh@users.noreply.github.com>
2021-04-24 15:19:46 +0530

    Initial Commit

--- Others/organize.py
+++ Others/organize.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     createIfNotExist('Docs')
     createIfNotExist('Media')
     createIfNotExist('Others')
+    createIfNotExist('Programming')
 
     imgExts = [".png", ".jpg", ".jpeg"]
     images = [file for file in files if os.path.splitext(file)[1].lower() in imgExts ]
@@ -27,17 +28,19 @@ if __name__ == "__main__":
     mediaExts = [".mp4", ".mp3", ".flv",".wav"]
     medias = [file for file in files if os.path.splitext(file)[1].lower() in mediaExts]
     
-    ProgExts = [".py", ".java", ".h",".html",".bat"]
-    Programming = [file for file in files if os.path.splitext(file)[1].lower() in ProgExts]
+    codeExts = [".py", ".java", ".h",".html",".bat",".md"]
+    Code = [file for file in files if os.path.splitext(file)[1].lower() in codeExts]
 
     others = []
     for file in files:
         ext = os.path.splitext(file)[1].lower()
-        if (ext not in mediaExts) and (ext not in docExts) and (ext not in imgExts) and os.path.isfile(file):
+        if (ext not in mediaExts) and (ext not in docExts) and (ext not in imgExts) and os.path.isfile(file) and (ext not in codeExts):
             others.append(file)
 
     move("Images", images)
     move("Docs", docs)
     move("Media", medias)
+    move("Programming",Code)
     move("Others", others)
+    
 
