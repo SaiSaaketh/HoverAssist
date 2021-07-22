f5ee95012389eadce631b7624a1a027c886eec07
SaiSaaketh <75871456+SaiSaaketh@users.noreply.github.com>
2021-07-22 08:31:23 +0530

    Initial Commit

---
+++ HoverUI.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from itertools import count, cycle
+ 
+class ImageLabel(tk.Label):
+    """
+    A Label that displays images, and plays them if they are gifs
+    :im: A PIL Image instance or a string filename
+    """
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        frames = []
+ 
+        try:
+            for i in count(1):
+                frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+        self.frames = cycle(frames)
+ 
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
+ 
+        if len(frames) == 1:
+            self.config(image=next(self.frames))
+        else:
+            self.next_frame()
+ 
+    def unload(self):
+        self.config(image=None)
+        self.frames = None
+ 
+    def next_frame(self):
+        if self.frames:
+            self.config(image=next(self.frames))
+            self.after(self.delay, self.next_frame)
+ 
+#demo :
+root = tk.Tk()
+lbl = ImageLabel(root)
+lbl.pack()
+lbl.load('C:\\Users\\JAGADEESWARARAO\\Downloads\\mylivewallpapers.com-Lets-Travel-One-Day.gif')
+root.mainloop()
